30b619c1515bac2df4d1c522074060cafef8f94a
saepulfariz <saepulfariz108@gmail.com>
2024-11-09 13:18:53 +0700

    add update example collection Operations

--- app/src/main/kotlin/com/dicoding/kotlin/CollectionsOperations.kt
+++ app/src/main/kotlin/com/dicoding/kotlin/CollectionsOperations.kt
@@ -56,4 +56,65 @@ fun main() {
     // Output: 3
 
     // Pada kode di atas konsol akan menampilkan 3 sebagai jumlah item yang merupakan kelipatan 3, yaitu: 3, 6, dan 9.
+
+
+    // find(), firstOrNull(), & lastOrNull()
+
+    // Selanjutnya adalah fungsi yang digunakan untuk mencari item pada sebuah collection. Untuk mencari item pertama yang sesuai dengan kondisi yang kita tentukan, kita bisa menggunakan fungsi find(). Contoh, kita perlu mencari angka ganjil pertama dari numberList kodenya akan seperti berikut.
+
+    // val numberList = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val firstOddNumber = numberList.find { it % 2 == 1 }
+    val firstOrNullNumber = numberList.firstOrNull { it % 2 == 3 }
+
+    println(numberList)
+    println(firstOddNumber)
+    println(firstOrNullNumber)
+
+    // firstOddNumber: 1
+    // firstOrNullNumber: null
+
+    // Fungsi find() ini memiliki cara kerja yang sama dengan fungsi firstOrNull(). Artinya, jika di dalam collection tidak ditemukan data yang sesuai, fungsi akan mengembalikan nilai null. Tidak seperti fungsi filter() atau map() yang akan melakukan iterasi terhadap seluruh item, fungsi find() dan firstOrNull() ini akan langsung mengembalikan nilai ketika kondisi terpenuhi. Kemudian, jika Anda ingin mencari item terakhir, gunakan fungsi lastOrNull().
+
+
+    // first() & last()
+
+    // Hampir sama seperti fungsi firstOrNull() dan lastOrNull(), fungsi first() dan last() digunakan untuk menyaring item pertama atau terakhir dari sebuah collection. Kita juga bisa menambahkan sebuah kondisi dengan parameter. Namun, perlu diperhatikan jika kita menambahkan kondisi padahal kondisi tersebut tidak terpenuhi, apa hasilnya? Exception! Lihat saja contohnya pada kode berikut.
+
+
+    // val numberList = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val numberFirst = numberList.first()
+    val numberLast = numberList.last()
+    println(numberFirst)
+    println(numberLast)
+    // val moreThan10 = numberList.first { it > 10 }
+    // println(moreThan10)
+
+    // Output: Exception in thread "main" java.util.NoSuchElementException: Collection contains no element matching the predicate.
+
+    // sum()
+    // Mungkin Anda sudah tahu fungsi ini. Fungsi sum() khusus hanya bisa digunakan untuk collection yang bertipe angka. Fungsi ini akan menjumlahkan setiap data yang ada pada collection.
+
+    // val numberList = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val total = numberList.sum()
+
+    // total: 55
+
+    println(total)
+
+    // sorted()
+    // sorted() digunakan untuk mengurutkan item yang ada di dalam collection. Secara default fungsi sorted() ini akan mengurutkan data secara ascending. Perhatikan kode di bawah ini.
+
+    val kotlinChar = listOf('k', 'o', 't', 'l', 'i', 'n')
+    val ascendingSort = kotlinChar.sorted()
+    println(ascendingSort)
+
+    // ascendingSort: [i, k, l, n, o, t]
+
+    // Namun, untuk sorting secara descending, kita dapat menggunakan fungsi sortedDescending().
+
+    // val kotlinChar = listOf('k', 'o', 't', 'l', 'i', 'n')
+    val descendingSort = kotlinChar.sortedDescending()
+    println(descendingSort)
+
+    // descendingSort: [t, o, n, l, k, i]
 }
